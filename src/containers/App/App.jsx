9e0c4748018ca1feef507df4bed48572880438e6
MansAhmadBONgit config --global user.name MansAhmadBONgit config --global user.name MansAhmadBON <mansue.ahmad.94@mail.ru>
2020-02-10 17:10:06 +0300

    feat: added playlist.json

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -1,10 +1,13 @@
 import React, {Component} from "react";
 import {connect} from 'react-redux';
 import {addPlayList} from '../../store/actions';
+import * as playlistJSON from '../../playlist';
 
 class App extends Component{
     componentDidMount() {
-        this.props.addPlayList([1,2,3,4,5,6,7,8,9,10])
+        const playlist = playlistJSON.default;
+        this.props.addPlayList(playlist);
+
     }
 
     render() {
